8378d6e5918c0badf136c1e9aa87c44025992804
Vianey Garcia <garcia_vianey01@student.smc.edu>
2019-11-15 23:51:04 -0800

    Week 12-HW Update: Modal/Caption slide
    Hover over image a drop down caption slides down.  Click the image a modal with the image appears.

--- week_12_homework/script.js
+++ week_12_homework/script.js
@@ -2,4 +2,35 @@ $(document).ready(function(){
     $('.imgToggle').click(function(){
         $('.icon').toggle();
     })
+
+    // Hover over image, caption slides up
+    $( "#gallery-image" ).hover(function() {
+          $('.caption').slideDown(1000);
+        },
+          function() {
+            $('.caption').slideUp(1000);
+        }
+      );
+
+
+    // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("gallery-image");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
 });
