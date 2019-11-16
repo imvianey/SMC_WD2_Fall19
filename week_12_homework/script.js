03cafd09b9edffa97b62e00221320cd8c5feadcc
Vianey Garcia <garcia_vianey01@student.smc.edu>
2019-11-16 03:02:01 -0800

    Week 12-HW Update
    Modal is scrollable if page is too long.  Added new elements to images in index page.

--- week_12_homework/script.js
+++ week_12_homework/script.js
@@ -1,36 +1,33 @@
-$(document).ready(function(){
-    $('.imgToggle').click(function(){
+$(document).ready(function () {
+    $('.imgToggle').click(function () {
         $('.icon').toggle();
     })
 
     // Hover over image, caption slides up
-    $( "#gallery-image" ).hover(function() {
-          $('.caption').slideDown(1000);
+    $(".gallery-01").hover(function () {
+            $('.caption').slideDown(1000);
         },
-          function() {
+        function () {
             $('.caption').slideUp(1000);
         }
-      );
+    );
 
+    // Modal:  Click gallery image,  image full screen
+    var modal = document.getElementById("myModal");
 
-    // Get the modal
-var modal = document.getElementById("myModal");
+    var img = document.getElementById("gallery-image");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    img.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("gallery-image");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+    // Close modal
+    var span = document.getElementsByClassName("close")[0];
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-} 
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
 });
