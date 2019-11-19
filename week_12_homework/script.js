df6eb67f04ef4b82f1b90516850737a2ae2ab1bd
Vianey Garcia <garcia_vianey01@student.smc.edu>
2019-11-19 07:47:39 -0800

    Week 12-HW Update-Fixed hover
    Jquery- Hover over imaged, caption drops down/up. Fixed
    
    Index- Changed image class, added capton and modal to all gallery images.
    
    main- h2 italic

--- week_12_homework/script.js
+++ week_12_homework/script.js
@@ -5,14 +5,14 @@ $(document).ready(function () {
 
     // Hover over image, caption slides up
     var captionDrop = $("#gallery").find(".caption");
-
-    $(".gallery-01, .gallery-02, .gallery-03").hover(function () {
-            $(captionDrop, this).slideDown(400);
-        },
-        function () {
-            $(captionDrop, this).slideUp(400);
-        }
-    );
+    $(".gallery-01, .gallery-02, .gallery-03, .gallery-04, .gallery-05, .gallery-06, .gallery-07, .gallery-08, .gallery-09, .gallery-10, .gallery-11, .gallery-12")
+        .hover(function () {
+                $(this).siblings(".caption").slideDown(400);
+            },
+            function () {
+                $(this).siblings(".caption").slideUp(400);
+            }
+        );
 
     // Modal:  Click gallery image,  image full screen
     var modal = document.getElementById("myModal");
