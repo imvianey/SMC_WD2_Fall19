58203529b1d882f13b07dfac5fef18de6d329c80
Vianey Garcia <garcia_vianey01@student.smc.edu>
2019-12-07 04:17:20 -0800

    Final Update- Donation
    Created and populated the donation page with headers, labels and input fields.
    
    CSS page for forms- has donation page css.
    
    Jquery for when donation amount selected.

--- westsidefoodbank/script.js
+++ westsidefoodbank/script.js
@@ -1,12 +1,19 @@
 $(document).ready(function () {
-    $(".menu").click(function(){
+    $(".menu").click(function () {
         $("nav a").toggleClass("hidden");
     })
 
     // CLICK, CAPTION DROPS
-$(".drop")
-    .click(function () {
+    $(".drop")
+        .click(function () {
             $(this).children(".caption").slideToggle("slow");
-        }
-    );
+        });
+
+    //Set & Highlight Donation Amount
+    $(".button").click(function () {
+        $(".button").removeClass("selected");
+        $(this).addClass("selected");
+
+        $(this).find("input").focus();
+    });
 });
