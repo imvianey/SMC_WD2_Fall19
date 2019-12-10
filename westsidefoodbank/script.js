277e5b61fb61a2c37d8ce446edc94e7fba1bcb66
Vianey Garcia <garcia_vianey01@student.smc.edu>
2019-12-10 14:28:51 -0800

    Final Update- Index Involved hover
    Hover over involved div elements and they change color and scale up.  Made involved div flex, changed height of div in media queries

--- westsidefoodbank/script.js
+++ westsidefoodbank/script.js
@@ -1,33 +1,54 @@
 $(document).ready(function () {
-    $(".icon").click(function () {
-        $(".nav-item").toggleClass("hidden");
-    })
 
-    $('.img-toggle').click(function () {
-      $('.icon').toggle();
+  // Nav icon toggle
+  $(".icon").click(function () {
+    $(".nav-item").toggleClass("hidden");
   })
 
-    // CLICK, CAPTION DROPS
-    $(".drop")
-        .click(function () {
-            $(this).children(".caption").slideToggle("slow");
-        });
+  $('.img-toggle').click(function () {
+    $('.icon').toggle();
+  })
 
-    //Set & Highlight Donation Amount
-    $(".button").click(function () {
-        $(".button").removeClass("selected");
-        $(this).addClass("selected");
+  // Image hover change
+  var sourceSwap = function () {
+    var $this = $(this);
+    var newSource = $this.data('alt-src');
+    $this.data('alt-src', $this.attr('src'));
+    $this.attr('src', newSource);
+  }
 
-        $(this).find("input").focus();
+  $('.hover-img').hover(sourceSwap, sourceSwap);
+
+  // Change H2 color
+  $('.make-red').mouseover(function () {
+    $('.red-h2', this).css("color", "#F25E5E");
+  })
+
+  $('.make-red').mouseleave(function () {
+    $('.red-h2', this).css("color", "#024873");
+  })
+
+  // CLICK, CAPTION DROPS
+  $(".drop")
+    .click(function () {
+      $(this).children(".caption").slideToggle("slow");
     });
 
-    // Replace form with replace message
-    $( ".donate-submit" ).click(function() {
-        $("form").replaceWith( $( ".replace" ) );
-      });
-    
-    //   Remove hidden class from replace message
-      $(".donate-submit").click(function(){
-        $(".replace").removeClass("hidden-all");
-      });
+  //Set & Highlight Donation Amount
+  $(".button").click(function () {
+    $(".button").removeClass("selected");
+    $(this).addClass("selected");
+
+    $(this).find("input").focus();
+  });
+
+  // Replace form with replace message
+  $(".donate-submit").click(function () {
+    $("form").replaceWith($(".replace"));
+  });
+
+  //   Remove hidden class from replace message
+  $(".donate-submit").click(function () {
+    $(".replace").removeClass("hidden-all");
+  });
 });
